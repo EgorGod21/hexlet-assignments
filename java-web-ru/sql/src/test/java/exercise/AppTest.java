e65e72fb0c0c5381c082ba0c4b2359a06a15c2d2
EgorGod21 <EgorGod21@github.com>
2023-07-03 15:46:35 +1000

    изменено

--- java-web-ru/sql/src/test/java/exercise/AppTest.java
+++ java-web-ru/sql/src/test/java/exercise/AppTest.java
@@ -24,7 +24,14 @@ class AppTest {
 
     private Connection connection;
 
+    @BeforeEach
+    public void init() throws SQLException, IOException {
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","postgres");
 
+        Statement statement = connection.createStatement();
+        String init = getFileContent("init.sql");
+        statement.execute(init);
+    }
 
     private String getFileContent(String fileName) throws IOException {
         Path pathToSolution = Paths.get(fileName).toAbsolutePath();
@@ -33,8 +40,41 @@ class AppTest {
 
     @Test
     void test() throws IOException, SQLException {
-        assertThat(true).isEqualTo(true);
+        Statement statement = connection.createStatement();
+        statement.setQueryTimeout(30);
+
+        String sql = getFileContent("solution.sql");
+        ResultSet rs = statement.executeQuery(sql);
+
+        List<Map<String, String>> actual = new ArrayList<>();
+
+        while (rs.next()) {
+            actual.add(Map.of(
+                "first_name", rs.getString("first_name"),
+                "birthday", rs.getString("birthday")
+                )
+            );
+        }
+
+        List<Map<String, String>> expected = List.of(
+            Map.of(
+                "first_name", "Arya",
+                "birthday", "2003-03-29 00:00:00"
+            ),
+            Map.of(
+                "first_name", "Brienne",
+                "birthday", "2001-04-04 00:00:00"
+            ),
+            Map.of("first_name", "Robb",
+                "birthday", "1999-11-23 00:00:00"
+            )
+        );
+
+        assertThat(actual).isEqualTo(expected);
     }
 
-
+    @AfterEach
+    public void closeConnection() throws SQLException {
+        connection.close();
+    }
 }
